7716e59ab0a95e6d18929ae7c4e904b86d327117
choonyip <47437742+choonyip@users.noreply.github.com>
2019-11-14 15:44:20 +0900

    Add files via upload

---
+++ bodytracking/build/CMakeFiles/bodytracking.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bodytracking.dir/main.c.o"
+  "bodytracking.pdb"
+  "bodytracking"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/bodytracking.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
